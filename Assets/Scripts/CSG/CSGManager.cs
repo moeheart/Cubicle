10d59e5f3a30d3278587956a782320c251de4c03
Shiliang <zuo.shiliang@gmail.com>
2017-08-10 10:42:01 -0500

    fix library

--- Assets/Scripts/CSG/CSGManager.cs
+++ Assets/Scripts/CSG/CSGManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using ConstructiveSolidGeometry;
+//using ConstructiveSolidGeometry;
 
 public class CSGManager : MonoBehaviour {
 
