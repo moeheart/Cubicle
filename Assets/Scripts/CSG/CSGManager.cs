13d9e233e8328a96a6af4937401381c39b3c3f6d
Shiliang <zuo.shiliang@gmail.com>
2017-08-14 10:59:19 -0500

    add meta files

--- Assets/Scripts/CSG/CSGManager.cs
+++ Assets/Scripts/CSG/CSGManager.cs
@@ -3,15 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using ConstructiveSolidGeometry;
 
+[RequireComponent(typeof(ObjectsManager))]
 public class CSGManager : MonoBehaviour {
 
-	public GameObject newObjectPrefab;
-	private List<GameObject> gameObjects = new List<GameObject>();
-
-	private static GameObject opA, opB;
+	public static ObjectsManager objectsManager {get; private set;}
 
 	public static void OnClickGameObject(GameObject gameObject) {
-		if (opA == null) {
+		/*if (opA == null) {
 			opA = gameObject;
 		}
 		else if (opB == null) {
@@ -20,23 +18,24 @@ public class CSGManager : MonoBehaviour {
 		else {
 			opA = opB;
 			opB = gameObject;
-		}
+		}*/
 	}
 
 	// Use this for initialization
 	void Start () {
-		opA = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		/*opA = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		opB = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 		opA.transform.position = Vector3.zero;
 		opB.transform.position = new Vector3(0.5f,0,0);
 		gameObjects.Add(opA);
-		gameObjects.Add(opB);
-
+		gameObjects.Add(opB);*/
+		objectsManager = this.GetComponent<ObjectsManager>();
+		objectsManager.LoadGameObjects();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space)) {
+		/*if (Input.GetKeyDown(KeyCode.Space)) {
 			//GameObject newGo = Instantiate(newObjectPrefab, Vector3.zero, Quaternion.identity) as GameObject;
 			CSG result= CSGOperations.Subtract(opA, opB);
 			GameObject newGo = Instantiate(newObjectPrefab, Vector3.zero, Quaternion.identity) as GameObject;
@@ -44,6 +43,6 @@ public class CSGManager : MonoBehaviour {
 				newGo.GetComponent<MeshFilter>().mesh = result.toMesh();
 			Destroy(opA);
 			Destroy(opB);
-		}
+		}*/
 	}
 }
---
+++ Assets/Scripts/CSG/ObjectsManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sabresaurus.SabreCSG;
+
+public class ObjectsManager : MonoBehaviour {
+
+	private List<GameObject> gameObjects;
+	private static GameObject opA, opB;
+
+	CSGModelBase csgModel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadGameObjects() {
+
+		csgModel = gameObject.AddComponent<CSGModelBase>();
+
+		// Random position for brush
+		Vector3 localPosition = Random.insideUnitCircle * 100;
+		// All brushes same size
+		Vector3 localSize = new Vector3(20,16,20);
+		GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		//GameObject newObject = csgModel.CreateBrush(obj, localPosition, localSize, Quaternion.identity);
+	}
+}
