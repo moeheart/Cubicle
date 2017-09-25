8b42ad6278bf201166e2685d03ac5decec4fc711
Shiliang <zuo.shiliang@gmail.com>
2017-09-25 16:24:13 +0800

    real-time hint on e

--- Assets/Scripts/3D World/DeviceOperator.cs
+++ Assets/Scripts/3D World/DeviceOperator.cs
@@ -4,19 +4,37 @@ using System.Collections;
 public class DeviceOperator : MonoBehaviour {
 	public float radius = 1.5f;
 
+	private GameObject hintPanel;
+	private bool canOperate;
+
 	// Use this for initialization
 	void Start () {
-	
+		hintPanel = GameObject.Find("Hint Panel");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.E)) {
-			Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+		Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+		canOperate = false;
+		foreach (Collider hitCollider in hitColliders) {
+			Vector3 direction = hitCollider.transform.position - this.transform.position;
+			if (Vector3.Dot(transform.forward, direction) > .5f) {
+				if (hitCollider.gameObject.GetComponent<TriggerDevice>() != null) {
+					canOperate = true;
+					hintPanel.SetActive(true);
+				}
+			}
+		}
+		if (canOperate == false) {
+			hintPanel.SetActive(false);
+		}
+		if (canOperate && Input.GetKeyDown(KeyCode.E)) {
 			foreach (Collider hitCollider in hitColliders) {
 				Vector3 direction = hitCollider.transform.position - this.transform.position;
 				if (Vector3.Dot(transform.forward, direction) > .5f) {
-					hitCollider.SendMessage("Operate", SendMessageOptions.DontRequireReceiver);
+					if (hitCollider.gameObject.GetComponent<TriggerDevice>() != null) {
+						hitCollider.SendMessage("Operate", SendMessageOptions.DontRequireReceiver);
+					}
 				}
 			}
 		}
