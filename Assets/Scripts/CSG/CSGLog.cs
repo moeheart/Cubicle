6a114423f3fea641bb500cb041239d9aabf6e2bb
Shiliang <zuo.shiliang@gmail.com>
2017-09-27 23:48:16 +0800

    ikafsj;olaidjf;lkbj;dlgij;a

--- Assets/Scripts/CSG/CSGLog.cs
+++ Assets/Scripts/CSG/CSGLog.cs
@@ -6,9 +6,9 @@ using System.IO;
 public static class CSGLog{
 	public static void Log(string path, int id, string log) {
 		if (!File.Exists(path)) {
-			File.AppendAllText(path, "RoomID, PuzzleType:  TimeSinceEnterRoom,  Action\n");
+			File.AppendAllText(path, "RoomID, PuzzleType:  TimeSinceGameStart, TimeSinceEnterLevel,  Action\n");
 		}
 		float t = Time.time - ObjectsManager.startTime;
-		File.AppendAllText(path, id + ", CSG: " + t + ", " + log + "\n");
+		File.AppendAllText(path, id + ", CSG: " + Time.time + ", " + t + ", " + log + "\n");
 	}
 }
