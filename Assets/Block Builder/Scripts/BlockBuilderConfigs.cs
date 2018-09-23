e894f42984fd1d500e18596c41ce1a28375c36f5
Shiliang <zuo.shiliang@gmail.com>
2018-09-23 13:57:33 -0500

    update block builder configs

--- Assets/Block Builder/Scripts/BlockBuilderConfigs.cs
+++ Assets/Block Builder/Scripts/BlockBuilderConfigs.cs
@@ -18,15 +18,11 @@ public class BlockBuilderConfigs{
 
 	public static Vector3 baseGridWorldPosition = Vector3.zero;
 
-	public static Vector3 topViewAngle = new Vector3(-90, 90, -90);
-
-	public static Vector3 frontViewAngle = new Vector3(0, 0, 0);
-
-	public static Vector3 rightViewAngle = new Vector3(0, 90, 0);
-
 	public static float sensitivityGyro = 8.0f;
 
 	public static float distanceToBaseGrid = 8.0f;
+
+	public static float cosineSimilarityLowerBound = 0.94f;
 
 	private static Vector3 cameraPos = new Vector3 (5, 4.5f, -8.5f);
 	private static Vector3 rotation = new Vector3 (20, -30, 0);
