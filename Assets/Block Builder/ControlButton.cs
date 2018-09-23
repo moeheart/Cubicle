b8b19e2153cc21faf1b005ccf99fa8eec472e600
Shiliang <zuo.shiliang@gmail.com>
2018-09-23 12:24:10 -0500

    add control button script

---
+++ Assets/Block Builder/ControlButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ControlButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+	private Button button;
+
+	private bool isPressed;
+
+	// Use this for initialization
+	void Start () {
+		isPressed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnPointerDown(PointerEventData eventData) {
+		isPressed = true;
+	}
+
+	public void OnPointerUp(PointerEventData eventData) {
+		isPressed = false;
+
+	}
+
+	public bool IsPressed() {
+		return isPressed;
+	}
+}
