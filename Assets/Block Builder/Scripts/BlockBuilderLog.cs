677e7e54ceed71b1f99b1c7bb4ed5fd18aa5a035
Shiliang <zuo.shiliang@gmail.com>
2018-09-24 19:20:55 -0500

    update add log file

--- Assets/Block Builder/Scripts/BlockBuilderLog.cs
+++ Assets/Block Builder/Scripts/BlockBuilderLog.cs
@@ -6,10 +6,10 @@ using System.IO;
 public static class BlockBuilderLog {
 
 	public static void Log(string path, int id, string log) {
-		/*if (!File.Exists(path)) {
-			File.AppendAllText(path, "RoomID, PuzzleType:  TimeSinceGameStart, TimeSinceEnterLevel,  Action\n");
+		if (!File.Exists(path)) {
+			File.AppendAllText(path, "levelId, PuzzleType:  TimeSinceGameStart, TimeSinceEnterLevel,  Action\n");
 		}
 		float t = Time.time - BaseGrid.startTime;
-		File.AppendAllText(path, id + ", Block Builder: " + Time.time + ", " + t + ", " +  log + "\n");*/
+		File.AppendAllText(path, id + ", Block Builder: " + Time.time + ", " + t + ", " +  log + "\n");
 	}
 }
