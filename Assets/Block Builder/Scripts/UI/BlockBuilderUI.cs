c2b4b580d60000a7aa5114172f6075c54e1f05fb
Shiliang <zuo.shiliang@gmail.com>
2018-09-27 10:11:17 -0500

    add block builder ui script

---
+++ Assets/Block Builder/Scripts/UI/BlockBuilderUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlockBuilderUI : MonoBehaviour {
+
+	public InputField inputField;
+	public Button resetLevelButton;
+
+	// Use this for initialization
+	void Start () {
+		inputField.onEndEdit.AddListener(delegate {SetParticipantName(inputField); });
+		resetLevelButton.onClick.AddListener(ResetLevel);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetParticipantName(InputField inputField) {
+		BlockBuilderConfigs.participantName = inputField.text;
+		Debug.Log("input field name:" + inputField.text);
+	}
+
+	private void ResetLevel() {
+		BlockBuilderConfigs.id = 1;
+	}
+}
