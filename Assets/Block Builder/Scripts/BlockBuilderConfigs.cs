fa80e7c57e002969021900764ae76cbe56a0383d
Shiliang <zuo.shiliang@gmail.com>
2018-09-27 10:12:51 -0500

    add rotation method / participant name

--- Assets/Block Builder/Scripts/BlockBuilderConfigs.cs
+++ Assets/Block Builder/Scripts/BlockBuilderConfigs.cs
@@ -16,6 +16,10 @@ public class BlockBuilderConfigs{
 	public static string blockBuilderTutorialSceneName = "Block Builder Tutorial";
 	public static string blockBuilderMainGameSceneName = "Block Builder Main Game";
 
+	public enum RotationMethod {Gyro, Button, GyroAndButton};
+
+	public static RotationMethod rotationMethod = RotationMethod.Button;
+
 	public static Vector3 baseGridWorldPosition = Vector3.zero;
 
 	public static float sensitivityGyro = 8.0f;
@@ -23,6 +27,8 @@ public class BlockBuilderConfigs{
 	public static float distanceToBaseGrid = 10.0f;
 
 	public static float cosineSimilarityLowerBound = 0.94f;
+
+	public static string participantName;
 
 	private static Vector3 cameraPos = new Vector3 (5, 4.5f, -8.5f);
 	private static Vector3 rotation = new Vector3 (20, -30, 0);
