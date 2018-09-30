fb0036b66cf4166169834bbe3d2a37e595c488cc
Shiliang <zuo.shiliang@gmail.com>
2018-09-30 11:35:25 -0500

    update log

--- Assets/Block Builder/Scripts/BlockBuilderLog.cs
+++ Assets/Block Builder/Scripts/BlockBuilderLog.cs
@@ -7,17 +7,19 @@ using System;
 public static class BlockBuilderLog { 
 
 	public static string logPath = Path.Combine(Application.persistentDataPath, "Logs/Block Builder");
-	public static string logFile = Path.Combine(logPath, "log.txt");
+	public static string logFile = Path.Combine(logPath, "log.csv");
 
 	public static void Log(int id, string log) {
 
 		Directory.CreateDirectory(logPath);
 		
 		if (!File.Exists(logFile)) {
-			File.AppendAllText(logFile, "levelId" + '\t' + "TimeSinceGameStart" + '\t' + "TimeSinceEnterLevel" + '\t' + "Action" + "\n");
+			File.AppendAllText(logFile, "name" + ", " + "rotationMethod" + ", " + "levelId" + ", " + "TimeSinceGameStart" + ", " + "TimeSinceEnterLevel" + ", " + "Action" + "\n");
 		}
 		float t = Time.time - BaseGrid.startTime;
-		File.AppendAllText(logFile, id + "\t" + Time.time + "\t" + t + ", " +  log + "\n");
+		// Debug.Log("rotationMethod:" + BlockBuilderConfigs.thisLevelRotationMethod.ToString());
+		File.AppendAllText(
+			logFile, BlockBuilderConfigs.participantName + ", " + BlockBuilderConfigs.thisLevelRotationMethod.ToString() + ", " + id + ", " + Time.time + ", " + t + ", " +  log + "\n");
 		
 	}
 }
