27d44107d99cff5d4a3efb2eef81f4a2650ab088
Shiliang <zuo.shiliang@gmail.com>
2018-09-30 11:35:51 -0500

    add configs

--- Assets/Block Builder/Scripts/BlockBuilderConfigs.cs
+++ Assets/Block Builder/Scripts/BlockBuilderConfigs.cs
@@ -20,15 +20,20 @@ public class BlockBuilderConfigs{
 
 	public static RotationMethod rotationMethod = RotationMethod.Button;
 
+	public static RotationMethod thisLevelRotationMethod;
+
 	public static Vector3 baseGridWorldPosition = Vector3.zero;
 
 	public static float sensitivityGyro = 8.0f;
 
-	public static float distanceToBaseGrid = 10.0f;
+	public static float distanceToBaseGrid = 13.0f;
 
 	public static float cosineSimilarityLowerBound = 0.94f;
 
 	public static string participantName;
+
+	public static int totalLevels = 20;
+	public static int switchLevelId = 16;
 
 	private static Vector3 cameraPos = new Vector3 (5, 4.5f, -8.5f);
 	private static Vector3 rotation = new Vector3 (20, -30, 0);
