8993595f135e619475329fe55deab3b89b73d74c
Shiliang <zuo.shiliang@gmail.com>
2018-09-30 11:35:01 -0500

    update rotate camera using button

--- Assets/Block Builder/Scripts/RotateCameraUsingButton.cs
+++ Assets/Block Builder/Scripts/RotateCameraUsingButton.cs
@@ -7,7 +7,9 @@ public class RotateCameraUsingButton : MonoBehaviour {
 	[SerializeField]
 	public ControlButton xUpButton, xDownButton, clockwiseYButton, counterClockwiseYButton;
 
-	private float xRotationPerFrame = 0.3f, yRotationPerFrame = 0.3f;
+	private float xRotationPerFrame = 0.5f, yRotationPerFrame = 0.5f;
+
+	private ViewType lastViewType = ViewType.None;
 
 	// Use this for initialization
 	void Start () {
@@ -18,18 +20,21 @@ public class RotateCameraUsingButton : MonoBehaviour {
 		float xRotation, yRotation;
 		if (xUpButton.IsPressed()) {
 			xRotation = xRotationPerFrame * BlockBuilderConfigs.sensitivityGyro;
+			BlockBuilderLog.Log(BlockBuilderManager.currentLevelId, "xUp Button Pressed");
 			if (ViewUtil.canRotateAroundXAxis(transform)) {
 				this.transform.Rotate(Vector3.right, xRotation);
 			}
 		}
 		if (xDownButton.IsPressed()) {
 			xRotation = xRotationPerFrame * BlockBuilderConfigs.sensitivityGyro;
+			BlockBuilderLog.Log(BlockBuilderManager.currentLevelId, "xDown Button Pressed");
 			if (ViewUtil.canRotateAroundXAxis(transform)) {
 				this.transform.Rotate(Vector3.right, -xRotation);
 			}
 		}
 		if (clockwiseYButton.IsPressed()) {
 			yRotation = yRotationPerFrame * BlockBuilderConfigs.sensitivityGyro;
+			BlockBuilderLog.Log(BlockBuilderManager.currentLevelId, "Clockwise Button Pressed");
 			if (ViewUtil.canRotateAroundYAxis(transform)) {
 				this.transform.Rotate(Vector3.up, yRotation);
 			}
@@ -39,6 +44,7 @@ public class RotateCameraUsingButton : MonoBehaviour {
 		}
 		if (counterClockwiseYButton.IsPressed()) {
 			yRotation = yRotationPerFrame * BlockBuilderConfigs.sensitivityGyro;
+			BlockBuilderLog.Log(BlockBuilderManager.currentLevelId, "counterClockwise Button Pressed");
 			if (ViewUtil.canRotateAroundYAxis(transform)) {
 				this.transform.Rotate(Vector3.up, -yRotation);
 			}
@@ -47,7 +53,30 @@ public class RotateCameraUsingButton : MonoBehaviour {
 			}
 		}
 		ViewUtil.PlaceCameraFromRotation(this.transform, BlockBuilderConfigs.distanceToBaseGrid);
+		LogCurrentPosition();
 	}
 
+	private void LogCurrentPosition() {
+		if (ViewUtil.IsAlignedWithFrontView(transform)) {
+			if (lastViewType != ViewType.FrontView) {
+				lastViewType = ViewType.FrontView;
+				BlockBuilderLog.Log(BlockBuilderManager.currentLevelId, "Transition to Front View");
+			}
+		}
+		if (ViewUtil.IsAlignedWithTopView(transform)) {
+			if (lastViewType != ViewType.TopView) {
+				lastViewType = ViewType.TopView;
+				BlockBuilderLog.Log(BlockBuilderManager.currentLevelId, "Transition to Top View");
+			}
+			
+		}
+		if (ViewUtil.IsAlignedWithRightView(transform)) {
+			if (lastViewType != ViewType.RightView) {
+				lastViewType = ViewType.RightView;
+				BlockBuilderLog.Log(BlockBuilderManager.currentLevelId, "Transition to Right View");
+			}
+
+		}
+	}
 
 }
